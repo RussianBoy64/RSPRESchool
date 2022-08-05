5bfd147c8a2509c55a2f42e4fe82f0a4a63b4a35
RussianBoy64 <kozlachkovvd@gmail.com>
2022-08-05 16:43:23 +0400

    feat: styles for stylerArrows, getNextSlide and getPrevSlide functions, right path for bg image.webp

--- momentum/src/modules/bg.js
+++ momentum/src/modules/bg.js
@@ -1,10 +1,12 @@
 import { getTimeOfDay, TIMEOFDAY } from './greeting'
 
 const GITHUBLINK =
-  'url(https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/'
+  'url(https://raw.githubusercontent.com/RussianBoy64/stage1-tasks/assets/images/'
 
 const body = document.querySelector('body')
-let bgNum = getRandomNum().toString().padStart(2, '0')
+const prevBtn = document.querySelector('.fa-chevron-left')
+const nextBtn = document.querySelector('.fa-chevron-right')
+let bgNum = getRandomNum()
 
 function getRandomNum(min = 1, max = 20) {
   return Math.floor(Math.random() * (max - min + 1)) + min
@@ -12,9 +14,22 @@ function getRandomNum(min = 1, max = 20) {
 
 function setBg() {
   const currentDayTime = TIMEOFDAY[getTimeOfDay()]
-  bgNum = getRandomNum().toString().padStart(2, '0')
 
-  body.style.backgroundImage = GITHUBLINK + `${currentDayTime}/${bgNum}.jpg)`
+  body.style.backgroundImage =
+    GITHUBLINK + `${currentDayTime}/${bgNum.toString().padStart(2, '0')}.webp)`
 }
 
-export { setBg }
+function getSlidePrev() {
+  bgNum = bgNum-- === 1 ? 20 : bgNum--
+  setBg()
+}
+
+function getSlideNext() {
+  bgNum = bgNum++ === 20 ? 1 : bgNum++
+  setBg()
+}
+
+prevBtn.addEventListener('click', getSlidePrev)
+nextBtn.addEventListener('click', getSlideNext)
+
+export { setBg, getSlidePrev, getSlideNext }

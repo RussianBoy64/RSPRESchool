cdb65e3b9dcab9a954e9e96fc71da5406cef8f6e
RussianBoy64 <kozlachkovvd@gmail.com>
2022-08-08 15:06:12 +0400

    feat: bgImg change after fully load

--- momentum/src/modules/bg.js
+++ momentum/src/modules/bg.js
@@ -1,11 +1,11 @@
 import { getTimeOfDay, TIMEOFDAY } from './greeting'
 
 const GITHUBLINK =
-  'url(https://raw.githubusercontent.com/RussianBoy64/stage1-tasks/assets/images/'
+  'https://raw.githubusercontent.com/RussianBoy64/stage1-tasks/assets/images/'
 
 const body = document.querySelector('body')
-const prevBtn = document.querySelector('.fa-chevron-left')
-const nextBtn = document.querySelector('.fa-chevron-right')
+const prevBtn = document.querySelector('.btn-left')
+const nextBtn = document.querySelector('.btn-right')
 let bgNum = getRandomNum()
 
 function getRandomNum(min = 1, max = 20) {
@@ -14,22 +14,38 @@ function getRandomNum(min = 1, max = 20) {
 
 function setBg() {
   const currentDayTime = TIMEOFDAY[getTimeOfDay()]
+  const bgImg = new Image()
+  bgImg.src =
+    GITHUBLINK + `${currentDayTime}/${bgNum.toString().padStart(2, '0')}.webp`
 
-  body.style.backgroundImage =
-    GITHUBLINK + `${currentDayTime}/${bgNum.toString().padStart(2, '0')}.webp)`
+  // change bgImg after its fully loaded
+  bgImg.addEventListener('load', () => {
+    body.style.backgroundImage = `url(${bgImg.src})`
+  })
+
+  // when bgImg fully changed add listenners for change
+  body.addEventListener('transitionend', () => {
+    prevBtn.addEventListener('click', getSlidePrev)
+    nextBtn.addEventListener('click', getSlideNext)
+  })
 }
 
 function getSlidePrev() {
+  // removeEventListener for fastclick change
+  prevBtn.removeEventListener('click', getSlidePrev)
+  nextBtn.removeEventListener('click', getSlideNext)
+
   bgNum = bgNum-- === 1 ? 20 : bgNum--
   setBg()
 }
 
 function getSlideNext() {
+  // removeEventListener for fastclick change
+  prevBtn.removeEventListener('click', getSlidePrev)
+  nextBtn.removeEventListener('click', getSlideNext)
+
   bgNum = bgNum++ === 20 ? 1 : bgNum++
   setBg()
 }
-
-prevBtn.addEventListener('click', getSlidePrev)
-nextBtn.addEventListener('click', getSlideNext)
 
 export { setBg, getSlidePrev, getSlideNext }

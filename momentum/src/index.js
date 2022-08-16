1be24c4d637686f6cca6ee066d56e9fe2aab021d
RussianBoy64 <kozlachkovvd@gmail.com>
2022-08-17 02:01:18 +0400

    refactor: clear unusable imports

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -3,16 +3,8 @@ import './scss/index.scss'
 
 // Import scripts
 import { User } from './modules/user'
-// import { showTime } from './modules/time'
-// import { showDate } from './modules/date'
-// import { showGreeting, setUserName } from './modules/greeting'
 import { setLocalStorage, getLocalStorage } from './modules/localStorage'
-// import { setBg } from './modules/bg'
-// import { getWeather } from './modules/weather'
-// import { getQuote } from './modules/quotes'
 import { loadPlaylist } from './modules/player'
-// import { settingsToggle, setUpSettings } from './modules/settings'
-
 import { setUpWidgets } from './modules/setUpWidgets'
 
 // Variables

5510d0da328478467a702ccdfb270961812da2cc
RussianBoy64 <kozlachkovvd@gmail.com>
2022-08-16 20:35:02 +0400

    fix: current hide widget css and adaptive styles

--- momentum/src/modules/settings.js
+++ momentum/src/modules/settings.js
@@ -1,9 +1,21 @@
 const settingsBtn = document.querySelector('.settings__btn')
 const settingContainer = document.querySelector('.settings__container')
+const backDrop = document.querySelector('.backdrop')
 
 function settingsToggle() {
   settingsBtn.classList.toggle('active')
   settingContainer.classList.toggle('show')
+  backDrop.classList.toggle('active')
+
+  backDrop.addEventListener('click', hideSettings)
+}
+
+function hideSettings() {
+  settingsBtn.classList.remove('active')
+  settingContainer.classList.remove('show')
+  backDrop.classList.remove('active')
+
+  backDrop.removeEventListener('click', hideSettings)
 }
 
 settingsBtn.addEventListener('click', settingsToggle)

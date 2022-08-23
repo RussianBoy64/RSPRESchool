dbd2789b86974220e7ea81fcdf50726d5ae2cac9
RussianBoy64 <kozlachkovvd@gmail.com>
2022-08-23 20:38:27 +0400

    feat: ancietns card and dificulty level html+css

---
+++ codejam-eldritch/src/modules/domNodes.js
@@ -0,0 +1,13 @@
+const body = document.querySelector('body')
+const gameTitle = document.querySelector('.header__gameTitle')
+const backdrop = document.querySelector('.backdrop')
+const loader = document.querySelector('.loader')
+
+const domNodes = {
+  body,
+  gameTitle,
+  backdrop,
+  loader,
+}
+
+export { domNodes }
--- codejam-eldritch/src/modules/startGame.js
+++ codejam-eldritch/src/modules/startGame.js
@@ -1,20 +1,8 @@
-import image from '../assets/home.png'
-
-const body = document.querySelector('body')
-const backdrop = document.querySelector('.backdrop')
-const loader = document.querySelector('.loader')
+import { domNodes } from './domNodes'
 
 function loadGame() {
-  // create bgImg
-  const bgImg = new Image()
-  // load bgImg
-  bgImg.src = image
-  // when img fully load show content
-  bgImg.addEventListener('load', () => {
-    body.style.backgroundImage = `url(${bgImg.src})`
-    backdrop.classList.add('transparent')
-    loader.classList.add('hidden')
-  })
+  domNodes.backdrop.classList.add('transparent')
+  domNodes.loader.classList.add('hidden')
 }
 
 function startGame() {}
